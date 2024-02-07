96458af016c4b42b3ca447e52c35c8a8dc7da201
Damicable <annobidiegwu@gmail.com>
2024-02-07 02:38:44 +0000

    Basic setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
-"""Module to bootstrap a Flask app using babel"""
-
-
+"""Babel config class"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
-class Config(object):
-    """Babel configuration"""
+class Config():
+    """Babel configuation"""
+
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
 
-babel = Babel(app)
 
-
-@app.route('/', strict_slashes=False)
+@app.route("/")
 def home() -> str:
-    """Home page"""
-    return render_template('1-index.html')
+    """index page"""
+    return render_template("1-index.html")
 
 
 if __name__ == '__main__':
