c289140968efd49e2527a8ea70ade9024efc0e20
Damicable <annobidiegwu@gmail.com>
2024-02-07 02:31:09 +0000

    Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""0-app flask module"""
-
+"""Basic flask application module"""
 from flask import Flask, render_template
-from typig import Any
-
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home() -> Any:
-    """Home page"""
-    return render_template('0-index.html')
+@app.route("/")
+def home() -> str:
+    """index page"""
+    return render_template("0-index.html")
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)
