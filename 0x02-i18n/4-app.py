27995746f52a302d9087236b43967eff99dbe521
Damicable <annobidiegwu@gmail.com>
2024-02-07 03:26:51 +0000

    Flask app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python3
-"""Flask app module"""
-from flask import Flask, render_template, request
+"""A Basic flask application"""
+from flask import Flask
+from flask import request
+from flask import render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config():
-    """Babel configuration"""
-
-    LANGUAGES = ["en", "fr"]
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
+app = Flask(__name__)
 app.config.from_object(Config)
 
-
-@app.route("/")
-def home() -> str:
-    """index page"""
-    return render_template("4-index.html")
+# Wrap the application with Babel
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Gets locale supported languages"""
+def get_locale():
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-    locale = request.args.get('locale')
-    if locale:
-        if locale in app.config["LANGUAGES"]:
-            return locale
 
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+@app.route('/', strict_slashes=False)
+def index():
+    """index page"""
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
